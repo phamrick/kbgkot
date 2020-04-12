734a905dc5d6ee2312b62053a9afe2898ea799fa
Paul M. Hamrick <phamrick@tesla.com>
2020-04-12 16:21:44 -0400

    copied early code from the chat app example, currently works for synchronzied card draws and dragging

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.use(express.static('images'))
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('instantiateCard', function(data){
+	  
+    socket.broadcast.emit('instantiateCard',  { nodeID: data.nodeID, 
+	                              cardKey: data.cardKey  });
+  });
+  
+  socket.on('dragKnode', function(data){
+	
+    socket.broadcast.emit('dragKnode',  { nodeID: data.nodeID,
+								x: data.x,
+								y: data.y});
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
